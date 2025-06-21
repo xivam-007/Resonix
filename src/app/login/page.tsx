0136f51882ea4826a514406f3f5523fb210dd9d6
xivam-007 <xivam007@gmail.com>
2025-06-21 22:08:05 +0530

    some tweaks in frontend

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,8 @@
 
 import LoginForm from '@/components/LoginForm';
 import { signIn } from 'next-auth/react';
+import { motion } from "motion/react";
+import { LampContainer } from "@/components/ui/lamp";
 
 const LoginPage = () => {
   const handleSubmit = async (formData: {
@@ -25,9 +27,26 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-4 border rounded-lg shadow">
-      <h2 className="text-2xl font-bold mb-4">Log In</h2>
-      <LoginForm onSubmit={handleSubmit} />
+    <div>
+      <LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-xl font-medium tracking-tight text-transparent md:text-4xl"
+      >
+        Connecting talent with opportunity — one login away.  
+      </motion.h1>
+      <div className="max-w-md mx-auto mt-10 p-4 border rounded-lg shadow">
+        <h2 className="text-2xl font-bold mb-4">Log In</h2>
+        <LoginForm onSubmit={handleSubmit} />
+      </div>
+    </LampContainer>
+      
     </div>
   );
 };
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,6 +3,8 @@
 
 import SignupForm from '@/components/SignupForm';
 import { useRouter } from 'next/navigation';
+import { motion } from "motion/react";
+import { LampContainer } from "@/components/ui/lamp";
 
 const SignupPage = () => {
   const router = useRouter();
@@ -30,10 +32,27 @@ const SignupPage = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-4 border rounded-lg shadow">
-      <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
-      <SignupForm onSubmit={handleSubmit} />
+    <div>
+      <LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-xl font-medium tracking-tight text-transparent md:text-4xl"
+      >
+        Your journey starts here.  <br /> Sign in to explore and connect.
+      </motion.h1>
+      <div className="max-w-md mx-auto mt-10 p-4 border rounded-lg shadow">
+        <h2 className="text-2xl font-bold mb-4">Signup</h2>
+        <SignupForm onSubmit={handleSubmit} />
+      </div>
+    </LampContainer>
     </div>
+    
   );
 };
 
